f9216540bb081f5292461c939c296a7a51bd9cf4
Ramesh <saravram@a078177d79a2.ant.amazon.com>
2022-02-04 01:31:07 -0500

    updated bash script in post-api-helper

--- sandbox/api_helpers/post-api-helpers.sh
+++ sandbox/api_helpers/post-api-helpers.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-account = $(aws sts get-caller-identity --query Account --output text)
-region = $(aws ec2 describe-availability-zones --query 'AvailabilityZones[0].[RegionName]' --output text)
-cidr = $(python ./python/source/get_cidr_range.py)
-aws ssm put-parameter --name /$account/$region/vpc/cidr --value $cidr
+aws iam create-role --role-name custom
