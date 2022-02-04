1a77e7639557d2cf3206adc9bb3226a440ce6b6e
Ramesh <saravram@a078177d79a2.ant.amazon.com>
2022-02-04 01:02:37 -0500

    included bash script in post-api-helper

--- sandbox/api_helpers/post-api-helpers.sh
+++ sandbox/api_helpers/post-api-helpers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Executing Post-API Helpers"
+account = $(aws sts get-caller-identity --query Account --output text)
+region = $(aws ec2 describe-availability-zones --query 'AvailabilityZones[0].[RegionName]' --output text)
+cidr = $(python ./python/source/get_cidr_range.py)
+aws ssm put-parameter --name /$account/$region/vpc/cidr --value $cidr
