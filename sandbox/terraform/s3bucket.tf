08f881dd24aa2319ddb4dbca55e77b9bed291ae9
saravram <45466472+saravram@users.noreply.github.com>
2024-02-28 14:54:11 -0500

    Update s3bucket.tf

--- sandbox/terraform/s3bucket.tf
+++ sandbox/terraform/s3bucket.tf
@@ -1,4 +1,4 @@
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
  # bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
