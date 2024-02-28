98e5424dabd0642e6e704154851c798afd248201
saravram <45466472+saravram@users.noreply.github.com>
2024-02-28 14:51:56 -0500

    Delete sandbox/terraform/s33.tf

--- sandbox/terraform/s33.tf
+++
@@ -1,9 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "test_bucket" {
- # bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
- bucket = "aft-sandbox-74969021937400000"
-  acl    = "private"
-
-provider = aws.ap_south_1
-}
