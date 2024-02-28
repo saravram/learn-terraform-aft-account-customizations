675a8ca3744dadbcad08e7a12cfebe0b94888431
saravram <45466472+saravram@users.noreply.github.com>
2024-02-28 18:29:03 -0500

    Delete sandbox/terraform/s3bucket.tf

--- sandbox/terraform/s3bucket.tf
+++
@@ -1,9 +0,0 @@
-# data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "testing_bucket" {
- # bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
- bucket = "aft-sandbox-749690219374ap-south-2"
-  acl    = "private"
-
-provider = aws.ap_south_2
-}
