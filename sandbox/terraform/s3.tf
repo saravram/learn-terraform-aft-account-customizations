c1ce23f10b56f5c43febee5437036f6c05ca9fae
saravram <45466472+saravram@users.noreply.github.com>
2024-02-28 14:33:11 -0500

    Update s3.tf

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -2,8 +2,6 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
  # bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
- bucket = "aft-sandbox-74969021937400000"
+ bucket = "aft-sandbox-749690219374"
   acl    = "private"
-
-provider = aws.ap_south_1
 }
